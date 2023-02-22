8db78fa610826f90940440fb86c6f215e0a98d4a
Muhammad Fahad Arif <85834194+mfahadarif85@users.noreply.github.com>
2023-02-22 20:03:05 +0500

    styling step 01

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,27 @@
+import Image from "next/image";
 export default function Home() {
-  return <h1 className='text-3xl font-bold underline'>Hello world!</h1>;
+  return (
+    <div className='flex justify-center bg-gray-300'>
+      <div className='order-last mx-4 self-center'>
+        <Image
+          src='/panaverse.png'
+          alt='logo'
+          height={80}
+          width={80}
+          className='rounded-full'
+        />
+      </div>
+      <div className='mx-4 self-center text-center'>
+        <h1 className='font-bold text-red-600 sm:text-2xl lg:text-6xl'>
+          Welcome to Panaverse
+        </h1>
+        <h2 className='font-semibold text-blue-600 sm:text-xl lg:text-3xl'>
+          A community of Web3 and Metaverse Developers
+        </h2>
+        <button className='my-4 rounded-lg border-2 border-black bg-red-900 px-4 py-2 text-white'>
+          Learn More
+        </button>
+      </div>
+    </div>
+  );
 }
