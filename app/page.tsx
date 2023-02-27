850ebe3476f5b05bb1f9a32602121fcbe865b523
Muhammad Fahad Arif <85834194+mfahadarif85@users.noreply.github.com>
2023-02-27 12:09:17 +0500

    chapter 3 lists

--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,58 @@
 import Image from "next/image";
 export default function Home() {
   return (
-    <div className='flex justify-center bg-gray-300'>
-      <div className='order-last mx-4 self-center'>
-        <Image
-          src='/panaverse.png'
-          alt='logo'
-          height={80}
-          width={80}
-          className='rounded-full'
-        />
+    <>
+      <div className='flex justify-center bg-gray-300'>
+        <div className='order-last mx-4 self-center'>
+          <Image
+            src='/panaverse.png'
+            alt='logo'
+            height={80}
+            width={80}
+            className='rounded-full'
+          />
+        </div>
+        <div className='mx-4 self-center text-center'>
+          <h1 className='text-2xl text-blue-800'>Welcome to Panaverse</h1>
+          <h2 className='font-semibold text-blue-600 sm:text-xl lg:text-3xl'>
+            A community of Web3 and Metaverse Developers
+          </h2>
+          <button className='my-4 rounded-lg border-2 border-black bg-red-900 px-4 py-2 text-white'>
+            Learn More
+          </button>
+        </div>
       </div>
-      <div className='mx-4 self-center text-center'>
-        <h1 className='font-bold text-red-600 sm:text-2xl lg:text-6xl'>
-          Welcome to Panaverse
-        </h1>
-        <h2 className='font-semibold text-blue-600 sm:text-xl lg:text-3xl'>
-          A community of Web3 and Metaverse Developers
-        </h2>
-        <button className='my-4 rounded-lg border-2 border-black bg-red-900 px-4 py-2 text-white'>
-          Learn More
-        </button>
+      <div>
+        <div className='mb-10'>
+          <button className='border border-black p-10'>One</button>
+          <button className='m-10 border border-black'>Two</button>
+          <button className='m-2 border-4 border-black p-2'>Three</button>
+        </div>
+        <div>
+          <button className='m-4 rounded-md border-2 border-black p-4'>
+            Four
+          </button>
+          <button className='m-4 rounded-2xl border-2 border-black p-4'>
+            Five
+          </button>
+          <button className='m-4 rounded-full border-2 border-black p-4'>
+            Six
+          </button>
+        </div>
       </div>
-    </div>
+      <div className='h-full bg-slate-500 text-center'>
+        <p className='text-base font-bold capitalize leading-normal tracking-widest text-white underline decoration-wavy decoration-2 selection:bg-red-400'>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad officia
+          eligendi quis, voluptates nobis possimus quo distinctio velit dolorem
+          voluptatum nihil tenetur sapiente doloribus, ab nostrum quia
+          aspernatur laudantium molestias.
+        </p>
+        <ul className='list-decimal'>
+          <li>Phase 1</li>
+          <li>Phase 2</li>
+          <li>Phase 3</li>
+        </ul>
+      </div>
+    </>
   );
 }
